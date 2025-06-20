446f717e268d849c835eb756a727f6f70c473e17
BE3026 <bharathdon2002@gmail.com>
2025-06-21 00:40:56 +0530

    Defect

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,7 @@ function App() {
         
       ) : (
         <>
-          <SignUp setIsAuth={setIsAuth} />
+          <SignUp setIsAuth={setIAuth} />
           <Login setIsAuth={setIsAuth} />
         </>
       )}
