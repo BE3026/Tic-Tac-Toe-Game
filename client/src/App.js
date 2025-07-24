7bd68e5fc8736a2b81e47c57045a18d29b8778f9
BE3026 <89187251+BE3026@users.noreply.github.com>
2025-07-24 20:45:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Cookies from "universal-cookie";
 import { useState } from 'react';
 import JoinGame from './components/JoinGame';
 function App() {
-  const api_key="2321321321";
+  const api_key="23213321";
   const cookies = new Cookies();
   const client=StreamChat.getInstance(api_key);
   const token = cookies.get("token");
